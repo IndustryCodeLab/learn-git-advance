f5b49c2099d1d63240f456794370ac29102fac9e
Jatin-11dev <jattfact@gmail.com>
2025-08-20 22:29:40 +0530

    new

---
+++ list.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        // Node Newnode(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        } else{
+            newNode->next=head;
+            head=newNode;
+        }
+    }
+    void push_back(int val){
+        Node*newNode=new Node(val);
+        if(head==NULL){
+          head=tail=newNode;
+        }
+        else{
+            tail->next=newNode;
+            tail=newNode;
+        }
+    } 
+    void pop_front(){
+       if(head==NULL){
+        cout<<"ll is empty\n";
+        return;
+       }
+       Node*temp=head;
+       head=head->next;
+       temp->next=NULL;
+       delete temp;
+    }
+    void print(){
+        Node*temp=head;
+        while(temp!=NULL){
+           cout<<temp->data<<"->";
+           temp=temp->next; 
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+
+int main()
+{
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_back(10);
+
+    ll.print();
+
+    return 0;
+}
