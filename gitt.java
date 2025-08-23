b52d99b9e47f72f0d8f7bc6919fe2ce4193f9086
divyanshi singh <parihardivyanshi79@gmail.com>
2025-08-23 18:08:10 +0530

    hello

--- gitt.java
+++ gitt.java
@@ -2,7 +2,7 @@ public class gitt{
     public static void main(String[] args) {
         int n=10;
         for(int i=0; i<n ; i++){
-            System.out.println(i+helllo);
+            System.out.println(i+"hell0");
         }
     }
 }
