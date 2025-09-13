e827435bb5526b68856eccc16bc0750795ef2da7
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2025-09-13 20:17:37 +0530

    string

---
+++ strin.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;   // input string
+
+    vector<int> freq(26, 0);
+
+    // frequency count
+    for (char c : s) {
+        freq[c - 'a']++;
+    }
+
+    string vowels = "aeiou";
+    int maxVowel = 0, maxConsonant = 0;
+
+    // check vowels and consonants
+    for (int i = 0; i < 26; i++) {
+        char ch = 'a' + i;
+        if (freq[i] > 0) {
+            if (vowels.find(ch) != string::npos) {
+                maxVowel = max(maxVowel, freq[i]);
+            } else {
+                maxConsonant = max(maxConsonant, freq[i]);
+            }
+        }
+    }
+
+    cout << (maxVowel + maxConsonant) << endl;
+
+    return 0;
+}
